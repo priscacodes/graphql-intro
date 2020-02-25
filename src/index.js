067a5cc25f24598fd2eccbab9ddc0a9cd4e1a0da
Lovisgod <olifedayo94@gmail.com>
2020-02-25 16:18:46 +0100

    nodemon setup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+
+// scalar type this are types that store single values
+// String, Boolean, Int, Float, ID
+
 // define types (schema)
 // putting ! at the end of our type means we are going to always get back a string.
 const typeDefs = `
   type Query {
+      id: ID!
       hello: String!
       name: String!
       location: String!
       bio: String!
+      employed: Boolean!
+      gpa: Float
+      age: Int!
   }
 `
 
 // resolvers are functions that get called when a query is made. each query with it's own function.
 const resolvers = {
     Query: {
+        id() {
+            return 'abc1234ghtyru556llkklkljkj5';
+        },
+        age() {
+            return 26;
+        },
         hello() {
             return 'This is the first query';
         },
@@ -25,6 +39,9 @@ const resolvers = {
         },
         bio() {
             return 'I eat and sleep with bugs'
+        },
+        gpa() {
+            return null;
         }
     }
 }
